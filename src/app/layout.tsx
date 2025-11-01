357d0ff50cc00b0a37fd30755a9741bf50fd3877
teresaxu <teresaxfy@gmail.com>
2025-10-31 20:45:13 -0700

    pwa setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
